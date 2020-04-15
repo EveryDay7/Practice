4bf455436285fd9a785e25deaa72f1333aa0dbb1
yangyaodong <yyd243639@163.com>
2020-04-15 17:13:42 +0800

    修改FTP工具类

--- demo1/src/main/java/com/yyd/demo1/util/FTPUtil.java
+++ demo1/src/main/java/com/yyd/demo1/util/FTPUtil.java
@@ -5,24 +5,26 @@ import org.apache.commons.net.ftp.FTPReply;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class FTPUtil {
+
     /** 日志对象 **/
     private static final Logger LOGGER = LoggerFactory.getLogger(com.yyd.demo1.util.FTPUtil.class);
 
     /** 本地字符编码  **/
-    private static String localCharset = "GBK";
+    //private static String localCharset = "GBK";
 
     /** FTP协议里面，规定文件名编码为iso-8859-1 **/
-    private static String serverCharset = "ISO-8859-1";
+    //private static String serverCharset = "ISO-8859-1";
 
     /** UTF-8字符编码 **/
-    private static final String CHARSET_UTF8 = "UTF-8";
+    //private static final String CHARSET_UTF8 = "UTF-8";
 
     /** OPTS UTF8字符串常量 **/
-    private static final String OPTS_UTF8 = "OPTS UTF8";
+    //private static final String OPTS_UTF8 = "OPTS UTF8";
 
     /** 设置缓冲区大小4M **/
     private static final int BUFFER_SIZE = 1024 * 1024 * 4;
@@ -83,7 +85,7 @@ public class FTPUtil {
      * @param ftpPath FTP服务器路径
      * @return String
      */
-    private static String changeEncoding(String ftpPath) {
+    /*private static String changeEncoding(String ftpPath) {
         String directory = null;
         try {
             if (FTPReply.isPositiveCompletion(ftpClient.sendCommand(OPTS_UTF8, "ON"))) {
@@ -94,56 +96,125 @@ public class FTPUtil {
             LOGGER.error("路径编码转换失败", e);
         }
         return directory;
-    }
+    }*/
 
     /**
-     *  上传单个文件
+     * 上传文件到FTP服务器,自动连接关闭ftp
      *
-     * @param address FTP服务器IP地址
-     * @param port FTP端口号,默认为 21
-     * @param username 账号
-     * @param password 密码
-     * @param ftpPath FTP服务器路径
-     * @param savePath 上传文件的本地路径
-     * @return boolean
-     * */
-    public boolean uploadFile(String address,int port,String username, String password,
-                              String ftpPath, String savePath){
-        login(address,port,username,password);
-        boolean b = uploadFile(ftpPath, savePath);
-        //boolean b = uploadSubdirectories(new File(savePath), ftpPath);
+     * @param address  FTP服务器IP地址
+     * @param port  服务器端口号,默认一般为 21
+     * @param username  账号
+     * @param password  密码
+     * @param remoteDirectoryPath FTP服务器文件相对路径，例如：test/123
+     * @param localPath 上传文件本地路径
+     * @return boolean 成功返回true，否则返回false
+     */
+    public boolean uploadFiles(String address,int port,String username, String password,
+                               String remoteDirectoryPath,String localPath){
+        login(address, port, username, password);
+        boolean uploadFiles = uploadFilesManual(remoteDirectoryPath, localPath);
         closeConnect();
-        return b;
+        return uploadFiles;
     }
 
-    public boolean uploadFile(String address,int port,String username, String password,
-                              String ftpPath, File file){
-        login(address,port,username,password);
-        boolean b = uploadFile(ftpPath, file);
-        //boolean b = uploadSubdirectories(file, ftpPath);
+    /**
+     * 上传文件到FTP服务器,自动连接关闭ftp
+     *
+     * @param address  FTP服务器IP地址
+     * @param port  服务器端口号,默认一般为 21
+     * @param username  账号
+     * @param password  密码
+     * @param remoteDirectoryPath FTP服务器文件相对路径，例如：test/123
+     * @param src 上传的文件
+     * @return boolean 成功返回true，否则返回false
+     */
+    public boolean uploadFiles(String address,int port,String username, String password,
+                               String remoteDirectoryPath,File src){
+        login(address, port, username, password);
+        boolean uploadFiles = uploadFilesManual(remoteDirectoryPath, src);
         closeConnect();
-        return b;
+        return uploadFiles;
     }
 
     /**
-     *  上传单个文件
+     * 上传文件到FTP服务器,需要手动连接关闭ftp
      *
-     * @param ftpPath FTP服务器路径
-     * @param savePath 上传文件的本地路径
-     * @return boolean
-     * */
-    public boolean uploadFile(String ftpPath, String savePath){
-        return uploadSubdirectories(new File(savePath),ftpPath);
+     * @param remoteDirectoryPath FTP服务器文件相对路径，例如：test/123
+     * @param localPath 上传文件本地路径
+     * @return boolean 成功返回true，否则返回false
+     */
+    public boolean uploadFilesManual(String remoteDirectoryPath,String localPath){
+        return uploadFilesManual(remoteDirectoryPath,new File(localPath));
     }
 
     /**
-     *  上传单个文件
+     * 上传文件到FTP服务器,需要手动连接关闭ftp
      *
-     * @param ftpPath FTP服务器路径
+     * @param remoteDirectoryPath FTP服务器文件相对路径，例如：test/123
+     * @param src 文件
+     * @return boolean 成功返回true，否则返回false
+     */
+    public boolean uploadFilesManual(String remoteDirectoryPath,File src) {
+        boolean fls = false;
+        if (ftpClient == null){
+            LOGGER.error("FTP连接异常!");
+            return fls;
+        }
+        if (!remoteDirectoryPath.endsWith("/")){
+            remoteDirectoryPath = remoteDirectoryPath.concat("/");
+        }
+        //  判断文件是否为目录
+        if (!src.isDirectory()){
+            //  非目录,开始上传
+            return uploadFile(remoteDirectoryPath,src);
+        }
+        String directoryPath = remoteDirectoryPath + src.getName() + "/";
+        try {
+            boolean makeDirFlag = createDirectorys(directoryPath);
+            /*LOGGER.info("文件名称 : " + src.getName());
+            LOGGER.info("FTP服务器目标相对路径 : " + remoteDirectoryPath);
+            LOGGER.info("创建并切换到目录 '" + directoryPath + "' : " + makeDirFlag);*/
+            if (!makeDirFlag){
+                return fls;
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.info(directoryPath + "目录创建失败");
+        }
+
+        //  获取该路径名表示的目录中的文件
+        File[] allFile = src.listFiles();
+        for (int currentFile = 0;currentFile < allFile.length;currentFile++) {
+            //  此抽象路径名表示的文件是否为目录
+            if (!allFile[currentFile].isDirectory()) {
+                //  非目录,开始上传文件
+                boolean uploadFile = uploadFile(directoryPath, allFile[currentFile]);
+                if (!uploadFile){
+                    return fls;
+                }
+            }
+        }
+        for (int currentFile = 0;currentFile < allFile.length;currentFile++) {
+            //  此抽象路径名表示的文件是否为目录
+            if (allFile[currentFile].isDirectory()) {
+                // 为目录,递归
+                boolean directory = uploadFilesManual(directoryPath, allFile[currentFile]);
+                if (!directory){
+                    return fls;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     *  上传单个文件(非目录)
+     *
+     * @param ftpPath FTP服务器路径,路径不存在则创建
      * @param file 上传的文件
      * @return boolean
-    * */
-    public boolean uploadFile(String ftpPath, File file){
+     * */
+    private boolean uploadFile(String ftpPath, File file){
         //return uploadSubdirectories(file, ftpPath);
         boolean flag = false;
         if (!ftpPath.endsWith("/")){
@@ -154,23 +225,24 @@ public class FTPUtil {
             try {
                 fis = new FileInputStream(file);
                 ftpClient.setBufferSize(BUFFER_SIZE);
-                // 设置编码：开启服务器对UTF-8的支持，如果服务器支持就用UTF-8编码，否则就使用本地编码（GBK）
-                if (FTPReply.isPositiveCompletion(ftpClient.sendCommand(OPTS_UTF8, "ON"))) {
+                //  设置编码：开启服务器对UTF-8的支持，如果服务器支持就用UTF-8编码，否则就使用本地编码（GBK）
+                /*if (FTPReply.isPositiveCompletion(ftpClient.sendCommand(OPTS_UTF8, "ON"))) {
                     localCharset = CHARSET_UTF8;
-                }
-                ftpClient.setControlEncoding(localCharset);
+                }*/
+                //ftpClient.setControlEncoding(localCharset);
+                //  设置传输模式
                 ftpClient.setFileType(FTPClient.BINARY_FILE_TYPE);
-                String path = changeEncoding(ftpPath);
                 // 目录是否存在
-                if (!ftpClient.changeWorkingDirectory(path)) {
-                    //this.createDirectorys(path);
-                    LOGGER.error("目录 " + path + " 不存在!");
-                    return flag;
+                if (!ftpClient.changeWorkingDirectory(ftpPath)) {
+                    LOGGER.error("上传目标目录不存在!");
+                    return false;
                 }
                 // 设置被动模式，开通一个端口来传输数据
                 ftpClient.enterLocalPassiveMode();
                 // 上传文件
-                flag = ftpClient.storeFile(new String(file.getName().getBytes(localCharset), serverCharset), fis);
+                flag = ftpClient.storeFile(new String(file.getName()), fis);
+                String info = flag ? "上传成功,文件名 :" : "上传失败,文件名 :";
+                LOGGER.info(info + file.getName());
             } catch (Exception e) {
                 LOGGER.error("本地文件上传FTP失败", e);
             }
@@ -178,158 +250,44 @@ public class FTPUtil {
         return flag;
     }
 
-
     /**
-     * 文件夹上传到FTP服务器
+     * 逐级创建并切换到指定目录
      *
-     * @param address FTP服务器IP地址
-     * @param port FTP端口号,默认为 21
-     * @param username 账号
-     * @param password 密码
-     * @param remoteDirectoryPath FTP服务器文件相对路径，例如：test/123
-     * @param localDirectory 文件夹路径
-     * @return boolean 成功返回true，否则返回false
-     */
-    public boolean uploadDirectory(String address,int port,String username, String password,
-                                   String remoteDirectoryPath,String localDirectory){
-        login(address,port,username,password);
-        boolean b = uploadDirectory(remoteDirectoryPath, localDirectory);
-        closeConnect();
-        return b;
-    }
-
-    public boolean uploadDirectory(String address,int port,String username, String password,
-                                   String remoteDirectoryPath,File src){
-        login(address,port,username,password);
-        boolean b = uploadDirectory(remoteDirectoryPath, src);
-        closeConnect();
-        return b;
-    }
-
-    /**
-     * 文件夹上传到FTP服务器
-     *
-     * @param remoteDirectoryPath FTP服务器文件相对路径，例如：test/123
-     * @param localDirectory 文件夹路径
-     * @return boolean 成功返回true，否则返回false
-     */
-    public boolean uploadDirectory(String remoteDirectoryPath,String localDirectory){
-        File src = new File(localDirectory);
-        return uploadDirectory(remoteDirectoryPath,src);
-    }
-
-    public boolean uploadDirectory(String remoteDirectoryPath,File src) {
-        if (!remoteDirectoryPath.endsWith("/")){
-            remoteDirectoryPath = remoteDirectoryPath.concat("/");
-        }
+     * @param path 需要创建并切换的目录
+     * @return boolean
+    * */
+    private boolean createDirectorys(String path){
+        boolean fls = false;
         try {
-            remoteDirectoryPath = remoteDirectoryPath + src.getName() + "/";
-            boolean makeDirFlag = this.ftpClient.makeDirectory(remoteDirectoryPath);
-            LOGGER.info("文件名称 : " + src.getName());
-            LOGGER.info("FTP服务器目标相对路径 : " + remoteDirectoryPath);
-            LOGGER.info("创建目录 " + src.getName() + " : " + makeDirFlag);
-            if (!makeDirFlag){
-                LOGGER.error("文件上传失败!");
+            if (!path.endsWith("/")){
+                path += "/";
+            }
+            if (ftpClient == null){
+                LOGGER.error("FtpClient 异常!");
                 return false;
             }
-        }catch (IOException e) {
-            e.printStackTrace();
-            LOGGER.info(remoteDirectoryPath + "目录创建失败");
-        }
-        File[] allFile = src.listFiles();
-        for (int currentFile = 0;currentFile < allFile.length;currentFile++) {
-            if (!allFile[currentFile].isDirectory()) {
-                String srcName = allFile[currentFile].getPath().toString();
-                uploadSubdirectories(new File(srcName), remoteDirectoryPath);
+            //  当输入的目录为根目录时的情况
+            if (path.equals("/")){
+                return ftpClient.changeWorkingDirectory(path);
             }
-        }
-        for (int currentFile = 0;currentFile < allFile.length;currentFile++) {
-            if (allFile[currentFile].isDirectory()) {
-                // 递归
-                uploadDirectory(allFile[currentFile].getPath().toString(),
-                        remoteDirectoryPath);
-            }
-        }
-        return true;
-    }
-
-    /**
-     * 文件上传到FTP服务器
-     *
-     * @param localFile 上传的文件
-     * @param romotUpLoadePath FTP服务器文件相对路径，例如：test/123
-     * @return boolean 成功返回true，否则返回false
-     */
-    private boolean uploadSubdirectories(File localFile, String romotUpLoadePath){
-        BufferedInputStream inStream = null;
-        boolean success = false;
-        try {
-            this.ftpClient.changeWorkingDirectory(romotUpLoadePath);// 改变工作路径
-            inStream = new BufferedInputStream(new FileInputStream(localFile));
-            LOGGER.info(localFile.getName() + "开始上传.....");
-            success = this.ftpClient.storeFile(localFile.getName(), inStream);
-            if (success == true) {
-                LOGGER.info(localFile.getName() + "上传成功");
-                return success;
-            }
-            LOGGER.error(localFile.getName() + "上传失败");
-        }catch (FileNotFoundException e) {
-            e.printStackTrace();
-            LOGGER.error(localFile + "未找到");
-        }catch (IOException e) {
-            e.printStackTrace();
-        }finally {
-            if (inStream != null) {
-                try {
-                    inStream.close();
-                }catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return success;
-    }
-
-    /**
-     * 在服务器上递归创建目录
-     *
-     * @param dirPath 上传目录路径
-     * @return
-     */
-    /*public void createDirectorys(String dirPath) {
-        try {
-            if (!dirPath.endsWith("/")) {
-                dirPath += "/";
-            }
-            String directory = dirPath.substring(0, dirPath.lastIndexOf("/") + 1);
-            ftpClient.makeDirectory("/");
-            int start = 0;
-            int end = 0;
-            if (directory.startsWith("/")) {
-                start = 1;
-            }else{
-                start = 0;
-            }
-            end = directory.indexOf("/", start);
-            while(true) {
-                String subDirectory = new String(dirPath.substring(start, end));
-                if (!ftpClient.changeWorkingDirectory(subDirectory)) {
-                    if (ftpClient.makeDirectory(subDirectory)) {
-                        ftpClient.changeWorkingDirectory(subDirectory);
-                    } else {
-                        LOGGER.info("创建目录失败");
-                        return;
+            String[] split = path.split("/");
+            String directory = "";
+            for (int i = 0; i < split.length ; i++){
+                directory += split[i] + "/";
+                //  判断目录是否存在,不存在则创建
+                if (!ftpClient.changeWorkingDirectory(directory)){
+                    if (!ftpClient.makeDirectory(directory)){
+                        LOGGER.error("创建目录 '" + directory + "' 失败!");
+                        return false;
                     }
                 }
-                start = end + 1;
-                end = directory.indexOf("/", start);
-                //检查所有目录是否创建完毕
-                if (end <= start) {
-                    break;
-                }
             }
-        } catch (Exception e) {
-            LOGGER.error("上传目录创建失败", e);
+            //  切换目录
+            fls = ftpClient.changeWorkingDirectory(path);
+        }catch (Exception e){
+            LOGGER.error("创建目录失败!",e);
         }
-    }*/
+        return fls;
+    }
+
 }
